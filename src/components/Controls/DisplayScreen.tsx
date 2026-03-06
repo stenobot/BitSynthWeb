3beb9144e98165ec6de22edd8e9b9bb40f454ab0
Andy <andrewmyers@gmail.com>
2026-03-05 17:41:02 -0800

    added versioning

--- src/components/Controls/DisplayScreen.tsx
+++ src/components/Controls/DisplayScreen.tsx
@@ -1,15 +1,37 @@
+import { useState, useEffect, useRef } from 'react'
 import { useSynthStore } from '../../store/synthStore'
 import { Visualizer } from './Visualizer'
+import { APP_VERSION } from '../../version'
 import './DisplayScreen.css'
 
 export function DisplayScreen() {
   const displayMessage = useSynthStore((state) => state.displayMessage)
+  const [showIntro, setShowIntro] = useState(true)
+  const introRef = useRef<HTMLSpanElement>(null)
+
+  useEffect(() => {
+    const el = introRef.current
+    if (!el) return
+
+    const handleAnimationEnd = () => {
+      setShowIntro(false)
+    }
+
+    el.addEventListener('animationend', handleAnimationEnd)
+    return () => el.removeEventListener('animationend', handleAnimationEnd)
+  }, [])
 
   return (
     <div className="display-screen">
-      <span className="display-screen__text">
-        {displayMessage}
-      </span>
+      {showIntro ? (
+        <span ref={introRef} className="display-screen__text display-screen__text--intro">
+          BitSynth v{APP_VERSION}
+        </span>
+      ) : (
+        <span className="display-screen__text">
+          {displayMessage}
+        </span>
+      )}
       <div className="display-screen__visualizer-container">
         <Visualizer />
       </div>
